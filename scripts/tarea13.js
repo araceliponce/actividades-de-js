4c3108dedcf1c49d4ca9f9f7a571f8188284565b
araceliponce <113866541+araceliponce@users.noreply.github.com>
2022-11-21 14:03:19 -0500

    tarea 13 y 15?

--- scripts/tarea13.js
+++ scripts/tarea13.js
@@ -1,1 +1,116 @@
 ///////////////////////////////////////////////////////
+
+let numUno = +prompt('Ingrese un primer numero');
+let numDos = +prompt('Ingrese un segundo numero');
+let suma = numUno + numDos;
+let producto = numUno * numDos;
+console.log(`La suma de tus números ${numUno} y ${numDos} es ${suma} y su producto es ${producto}`);
+
+
+let numEntero = +prompt('Ingrese un numero entero');
+let porcentaje = numEntero * 0.2;
+console.log(`El 10% de ${numEntero} es ${porcentaje}`);
+
+
+let nMetros = +prompt('Ingrese un número de metros');
+let equivCM = nMetros * 100;
+let equivMM = equivCM * 10;
+
+console.log(`${nMetros} metros es igual a ${equivCM} cm y es igual a ${equivMM} milímetros`);
+
+
+
+let base = +prompt('Ingrese la base');
+let altura = +prompt('Ingrese la altura');
+let area = base * altura / 2;
+
+
+console.log(`el área del triángulo es ${area}`);
+console.log({ base }, { altura }, { area });
+
+
+
+let nDolares = +prompt('número de dólares');
+let nSoles = nDolares * 3.38;
+
+console.log(`equivalen a ${nSoles}`);
+console.log({ nDolares });
+
+
+
+let nPulgadas = +prompt('número de pulgadas');
+let nMilimetros = nDolares * 25.4;
+
+console.log(`equivalen a ${nMilimetros} mm`);
+console.log({ nPulgadas });
+
+
+let otroNumero = prompt('un número de tres cifras');
+
+let digits = otroNumero.split('');
+console.log({ digits });
+let digitsN = digits.map(Number); //https://stackoverflow.com/questions/7784620/javascript-number-split-into-individual-digits para volverlos numeros
+console.log({ digitsN });
+
+let otraSuma = digitsN.reduce((a, b) => a + b, 0);
+let resultadoFinal = otraSuma ** 2 //es igual a Math.pow(otraSuma);
+
+console.log(`la suma de sus tres cifras es ${otraSuma} y esa suma al cuadrado es ${resultadoFinal}`);
+
+
+
+
+let monto = +prompt('Ingresa un monto');
+
+let paraA = monto * 0.3;
+let paraB = monto * 0.2;
+let paraC = monto * 0.5;
+
+console.log(`Al socio A le corresponden ${paraA}, al socio B ${paraB} y al socio C ${paraC}`);
+
+
+
+const igv = 0.19;
+let precioBase = +prompt('ingrese el precio base');
+let igvPrecioBase = (precioBase * igv).toFixed(2);
+
+let precioFinal = precioBase + igvPrecioBase;
+
+console.log(`el precio final por pagar es ${precioFinal}`);
+console.log({ precioBase }, { igvPrecioBase }, { precioFinal });
+
+
+
+let tiposEntradas = [['general', 150], ['para mayores de 65', 50], ['para menores de edad', '80']];
+let cuantas;
+let recaudado;
+let recaudados = [];
+let totalRecaudado;
+
+preguntarCalcularRecaudaciones();
+function preguntarCalcularRecaudaciones() {
+  tiposEntradas.forEach(tipo => {
+    cuantas = +prompt(`¿cuantas entradas del tipo '${tipo[0]}' se vendieron?`);
+    recaudado = cuantas * tipo[1];
+    console.log(`las ${cuantas} entradas de tipo ${tipo[0]} (a ${tipo[1]} soles cada una) recaudaron ${recaudado} soles `);
+
+    recaudados.push(recaudado);
+  })
+  console.log({ recaudados });
+  totalRecaudado = recaudados.reduce((a, b) => a + b, 0);
+  console.log(`En total se recaudaron ${totalRecaudado} soles`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/tarea15.js
+++ scripts/tarea15.js
@@ -1,1 +1,57 @@
-///////////////////////////////////////////////////////
+//RETO/////////////////////////////////////////////////////
+
+//entrada
+let notaParcial, notaExamen, notaPracticas, promedio, estado;
+let btnDos = document.getElementById('calcular');
+let resultadoDos = document.getElementById('resultadoDos');
+
+//procesos
+btnDos.addEventListener('click', () => {
+  nombre = document.getElementById('nombre').value;
+  notaParcial = document.getElementById('notaParcial').value;
+  notaExamen = document.getElementById('notaExamen').value;
+  notaPracticas = document.getElementById('notaPracticas').value;
+
+  promedio = (+notaParcial + notaExamen * 2 + +notaPracticas) / 4; //encontre que puedes anteceder un '+' para convertir un objeto de tipo texto a numero
+  promedio = promedio.toFixed(2); //reducimos los decimales aqui para no alargar la linea anterior
+  console.log({ promedio })
+
+  //salida basada en condicional
+  if (promedio > 13) {
+    estado = `${nombre} ha aprobado con un promedio de ${promedio}`;
+  } else {
+    estado = `${nombre} ha desaprobado con un pomedio de ${promedio}`;
+  }
+  resultadoDos.innerHTML = estado;
+});
+
+
+
+
+
+//entrada
+let btnUno = document.querySelector('#btn');
+let importeInicial, descuento, importeFinal, resultado1; //solo en cuando haga click al botón obtenemos los valores
+
+btnUno.addEventListener('click', () => {
+  importeInicial = document.getElementById('input1').value;
+  resultado1 = document.getElementById('resultado1');
+  descuento = 0;
+  importeFinal;
+
+  //procesos
+  if (importeInicial >= 150) {
+    descuento = (importeInicial * 0.12).toFixed(2);
+    importeFinal = importeInicial - descuento;
+
+    //salida
+    resultado1.innerHTML = `Su importe final es ${importeFinal} (se le descontaron ${descuento} a su importe inicial)`;
+
+  } else if (importeInicial > 0) {
+    //salida
+    resultado1.innerHTML = `Su importe final es ${importeInicial} (igual a su importe inicial)`;
+  }
+})
+
+
+//ACTIVIDAD ASINCRONA/////////////////////////////////////////////////////
