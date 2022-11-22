148d94f05643eee6d0187fd49c351d04e42df8e4
araceliponce <113866541+araceliponce@users.noreply.github.com>
2022-11-22 08:07:45 -0500

    commit

--- scripts/tarea16.js
+++ scripts/tarea16.js
@@ -1,44 +1,54 @@
-function tarea16act3(){
+function tarea16act3() {
   let numeroA = parseInt(prompt('introduzca un numero'));
   let numeroB = parseInt(prompt('introduzca un segundo numero'));
-  
-  //alertarSuma(numeroA, numeroB);
-  console.log(alertarSuma(numeroA, numeroB));
+
+  let laSuma = alertarSuma(numeroA, numeroB);
+  alert(`La suma de los dos numero es ${laSuma}`)
+  console.log(`La suma de los dos numero es ${laSuma}`);
   function alertarSuma(x, y) {
     return alert(x + y);
   };
 }
 
 
-function tarea16act2(){
+function tarea16act2() {
   let pasaraFarenheit = (grado) => { return farenheit = (grado * 1.8) + 32; };
 
+  let gradosUsuario = +prompt('Introduzaca una temperatura en Farenheit para convertirla en grados Celsius')
+
   var resultado = pasaraFarenheit(22); //71.6
-  //alert(resultado);
   console.log({ resultado })
-  
+
   let pasaraCelsius = (grado) => { return (grado - 32) / 1.8; };
-  
-  let temperatura = pasaraCelsius(72); //22.222222222222
-  //alert(temperatura);
+
+  let temperatura = pasaraCelsius(gradosUsuario); //22.222222222222
+
+  alert(`${gradosUsuario} grados Fahrenheit equivalen a ${temperatura} grados Celsius`);
 }
 
 
 
-function tarea16act1(){
+function tarea16act1() {
   let multiply = function (x, y) {
     return x * y;
   }
   let expon = function (x, y) {
     return Math.pow(x, y);
   }
-  
+
   let suma = (x, y) => { return x + y };
-  
-  
+
+
+  // funcion resta creada
   let resta = (x, y) => { return x - y };
-  var result = resta(5, 2); //para acceder a 'resta' debe estar despues de que se inicialize let resta
-  //alert(result);
-  console.log({ result });
+
+  // numeros obtenidos de prompts
+  let numX = +prompt('ingrese un primer número');
+  let numY = +prompt('ingrese un segundo número');
+
+  var result = resta(numX, numY);
+  let mensajeResta = `La resta de ${numX} y ${numY} es ${result}`
+  alert(mensajeResta);
+  console.log({ mensajeResta });
 }
 
